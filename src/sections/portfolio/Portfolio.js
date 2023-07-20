44610d54b0aaf21f73f9b799d3f94ba44b0df77c
jeremiah-wa <jeremiahwa98@gmail.com>
2023-07-20 21:58:50 +0100

    update Portfolio section

--- src/sections/portfolio/Portfolio.js
+++ src/sections/portfolio/Portfolio.js
@@ -3,6 +3,11 @@ import './Portfolio.css'
 
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+      <h2>Recent Projects</h2>
+      <p>
+      Check out some of the projects I recently worked on for my clients. Use the buttons to toggle the different categories.
+      </p>
+    </section>
   )
 }
