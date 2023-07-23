89df22435dc035e850128a3315cb420249337c6e
jeremiah-wa <jeremiahwa98@gmail.com>
2023-07-23 20:57:04 +0100

    Updated Entertaiment section and cards

--- src/sections/entertainment/Entertainment.js
+++ src/sections/entertainment/Entertainment.js
@@ -1,7 +1,31 @@
+import Card from '../../components/card/Card'
+import Carousel from '../../components/carousel/Carousel'
 import './Entertainment.css'
 import data from './data'
+import starImage from '../../assets/star.png'
 
 
+const Item = ({item}) => {
+  return (
+    <Card onClick={() => {
+        window.open(item.link, "_blank", "popup=false noopner noreferrer")
+    }}>
+        {item.image &&
+            <img className='poster' src={item.image} alt="Carousel Item Image" />
+        }
+        <h3 className='title'>{item.title}</h3>
+        
+        <p className='description'>
+          {item.description.substring(0, 100).trimEnd() + '....'}
+        </p>
+        <span className='rating'>
+          <img className='starImage' src={starImage} />
+          <h3>{item.rating}</h3><h4>/{item.ratingOutOf}</h4>
+        </span>
+        
+    </Card>
+  )
+}
 
 export default function Entertainment() {
   
@@ -11,9 +35,7 @@ export default function Entertainment() {
       <p>
       Check out some of the recent movies and shows I'm watching and some of the books I've read!
       </p>
-      <p>
-        {data[6].description.substring(0, 200).trimEnd() + '....'}
-      </p>
+      <Carousel data={data} Item={Item}/>
     </section>
   )
 }
