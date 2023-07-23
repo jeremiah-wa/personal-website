b4a6c6e7118ea495aead77efabaa2ab6237384e8
jeremiah-wa <jeremiahwa98@gmail.com>
2023-07-23 20:57:38 +0100

    Updated Portfolio section and cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Header from './sections/header/Header';
 import Portfolio from './sections/portfolio/Portfolio';
+import Entertainment from './sections/entertainment/Entertainment'
 import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
 import About from './sections/about/About'
--- src/sections/portfolio/Portfolio.js
+++ src/sections/portfolio/Portfolio.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import './Portfolio.css'
+import useRepos  from '../../hooks/useRepos'
+import Carousel from '../../components/carousel/Carousel'
+import Card from '../../components/card/Card'
+
+
+const Item = ({item}) => {
+  return (
+    <Card onClick={() => {
+        window.open(item.link, "_blank", "popup=false noopner noreferrer")
+    }}>
+        <h4>{item.title}</h4>
+        {item.description &&
+          <p className='description'>
+            {item.description.substring(0, 100).trimEnd() + '....'}
+          </p>
+        }
+        {item.topics &&
+          <div className='topic__wrapper'>
+            {item.topics.slice(0, 3).map(topic => <div className='topic'>{topic}</div>)}
+          </div>
+        }
+
+    </Card>
+  )
+}
 
 export default function Portfolio() {
+  const {repos} = useRepos()
+  
   return (
     <section id="portfolio">
       <h2>Recent Projects</h2>
       <p>
       Check out some of the projects I recently worked on for my clients. Use the buttons to toggle the different categories.
       </p>
+      <Carousel data={repos} Item={Item}/>
     </section>
   )
 }
